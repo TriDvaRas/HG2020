2073551b6adee799482816e104d00618fdeb5fd0
TriDvaRas <02dimon02@gmail.com>
2020-12-24 19:54:08 +0200

    Update Main.jsx

--- src/comp/Main.jsx
+++ src/comp/Main.jsx
@@ -24,6 +24,7 @@ function Main(props) {
         // creation
         <div className="mainContainer ">
             <div className="mainHeader  RoadRage yellow">Хуйня Года 2020</div>
+            <h1>Много букав </h1>
             <div className="commentary ">Госпади, а куда я попал?</div>
             {/* <div className="description ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
